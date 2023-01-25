5d9a592b19620da52caae269bbffdede1d234196
AlyssaD <pink_lime@live.com>
2023-01-25 13:10:36 -0500

    add heading image to each page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,12 @@
+import BackgroundImg2 from "../../components/BackgroundImg2/BackgroundImg2"
+import Footer from "../../components/Footer/Footer"
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <BackgroundImg2 heading='ABOUT' text='Get to know me' />
+      <Footer />
+    </div>
   )
 }
 
